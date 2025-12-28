9fb73ec09ce9d4e68c0cecc05564ed4eefa5b7cb
captain34643875-wq <captain34643875@gmail.com>
2025-12-28 21:06:30 +0900

    1116, 1117번 문제 풀이

---
+++ 입출력문 및 연산자/1116.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int i, a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d+%d=%d\n",a,b,a+b);
+    printf("%d-%d=%d\n",a,b,a-b);
+    printf("%d*%d=%d\n",a,b,a*b);
+    printf("%d/%d=%d\n",a,b,a/b);
+
+    return 0;
+}
