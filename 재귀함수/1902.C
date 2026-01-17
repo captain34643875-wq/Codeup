958d848bd4e17935fb076c6fa31b2a35d8e013ac
captain34643875-wq <captain34643875@gmail.com>
2026-01-17 23:18:16 +0900

    1902번 문제 풀이

---
+++ 재귀함수/1902.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void f(int n)
+{
+    if (n <= 0)
+        return;
+
+    printf("%d\n", n);
+    f(n - 1);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    f(n);
+    return 0;
+}
