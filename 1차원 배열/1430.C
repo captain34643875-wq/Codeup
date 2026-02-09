dada348c1d93b7d1df48dca31d8eff935d679d8b
captain34643875-wq <captain34643875@gmail.com>
2026-02-09 21:53:52 +0900

    1216번 풀이

---
+++ 1차원 배열/1430.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n,s;
+    int arr[10000001];
+    int arr2[10000001];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        
+    }
+
+    return 0;
+}
+    
---
+++ if~else/1216.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(b - c > a)
+        printf("advertise\n");
+    else if(b - c < a)
+        printf("do not advertise\n");
+    else
+        printf("does not matter\n");
+    return 0;
+}
